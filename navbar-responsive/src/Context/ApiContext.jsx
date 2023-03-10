5f7a12179e010c170be6dbe97b7337cc3eb8e05d
Julian <julianarg235@gmail.com>
2023-03-10 13:35:42 -0300

    toda la app de react

---
+++ navbar-responsive/src/Context/ApiContext.jsx
@@ -0,0 +1,13 @@
+import  {createContext} from 'react';
+
+
+const ApiContext = createContext({
+    // Array de productos
+    items: [],
+    email: "",
+    name: "",
+    phone: "",
+    lastPurchase: "",
+});
+
+export default ApiContext;
